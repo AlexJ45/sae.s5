33470ef004699432a7ef42febc41979a50d74e31
unknown <sophie.garreta@gmail.com>
2023-09-27 10:57:32 +0200

    init

---
+++ src/templates/etu/dashboard.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dashboard étudiant</title>
+</head>
+<body>
+    //traitement php
+    <p> nom prénom étu </p>
+</body>
+</html>
